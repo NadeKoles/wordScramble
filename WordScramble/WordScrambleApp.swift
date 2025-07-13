6f1985fbbc7e319a5e3d13567286c481428892de
NadeKoles <enot9296@gmail.com>
2025-07-13 21:12:47 +0300

    Initial commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Nadia on 12/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
